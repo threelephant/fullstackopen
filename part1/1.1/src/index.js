dcca9a17c11533bd6cf2ffe3ca17a5fd2772d64e
Peter <psl4@tpu.ru>
2020-01-16 23:55:26 +0700

    Update index.js

--- part1/1.1/src/index.js
+++ part1/1.1/src/index.js
@@ -4,30 +4,49 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
+const Header = (props) => (
+    <>
+        <h1>{props.course}</h1>
+    </>
+)
+
+const Parts = (props) => (
+    <>
+        <p>
+            {props.part} {props.exercise}
+        </p>
+    </>
+)
+
+const Content = (props) => (
+    <>
+    
+        <Parts part={props.parts[0]} exercise={props.exercises[0]}/>
+        <Parts part={props.parts[1]} exercise={props.exercises[1]}/>
+        <Parts part={props.parts[2]} exercise={props.exercises[2]}/>
+    </>
+)
+
+const Footer = (props) => (
+    <>
+        <p>
+            Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}
+        </p>
+    </>
+)
+
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const parts = ['Fundamentals of React',
+                    'Using props to pass data',
+                    'State of a component']
+    const exercises = [10, 7, 14]
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>
-                Number of exercises {exercises1 + exercises2 + exercises3}
-            </p>
+            <Header course={course} />
+            <Content parts={parts} exercises={exercises}/>
+            <Footer exercises={exercises}/>
         </div>
     )
 }
