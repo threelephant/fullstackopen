b76c7a683407869074d42f5b546ce0b336799561
Peter <psl4@tpu.ru>
2020-01-16 22:50:02 +0700

    Create React

---
+++ part1/1.1/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+    return (
+        <div>
+            <h1>{course}</h1>
+            <p>
+                {part1} {exercises1}
+            </p>
+            <p>
+                {part2} {exercises2}
+            </p>
+            <p>
+                {part3} {exercises3}
+            </p>
+            <p>
+                Number of exercises {exercises1 + exercises2 + exercises3}
+            </p>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
