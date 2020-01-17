fc4e3f9db6aee279004b367864012ecc7b2cebfc
Peter <psl4@tpu.ru>
2020-01-17 14:14:03 +0700

    Update file.js

--- part1/1.2/file.js
+++ part1/1.2/file.js
@@ -1,9 +1,10 @@
-const x = 1
-let y = 5
+const t = [1, -1, 3]
 
-console.log(x, y)
-y += 10
-console.log(x, y)
-y = 'sometext'
-console.log(x, y)
-x = 4
+t.push(5)
+
+console.log(t.length)
+console.log(t[1])
+
+t.forEach(value => {
+    console.log(value)
+})
