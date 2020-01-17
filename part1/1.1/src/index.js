616a07c974ddfe2caed97c33af57dc77cba57326
Peter <psl4@tpu.ru>
2020-01-17 15:30:40 +0700

    Update index.js

--- part1/1.1/src/index.js
+++ part1/1.1/src/index.js
@@ -4,35 +4,31 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-const Header = (props) => (
-    <>
-        <h1>{props.course}</h1>
-    </>
-)
+const Header = (props) => {
+    console.log(props)
+    return <h1>{props.course}</h1>
+}
 
-const Part = (props) => (
-    <>
-        <p>
-            {props.part} {props.exercise}
-        </p>
-    </>
-)
+const Part = (props) => {
+    console.log(props)
+    return <p>{props.part} {props.exercise}</p>
+}
 
-const Content = (props) => (
+const Content = (props) => {
+    console.log(props)
+    return (
     <>
         <Part part={props.parts[0]} exercise={props.exercises[0]}/>
         <Part part={props.parts[1]} exercise={props.exercises[1]}/>
         <Part part={props.parts[2]} exercise={props.exercises[2]}/>
     </>
-)
+    )
+}
 
-const Footer = (props) => (
-    <>
-        <p>
-            Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}
-        </p>
-    </>
-)
+const Footer = (props) => {
+    console.log(props)
+    return <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+}
 
 const App = () => {
     const course = 'Half Stack application development'
