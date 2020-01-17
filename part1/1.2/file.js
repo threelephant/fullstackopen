14ff91cf19e4a2bb5751d049289ea3b3b4d41505
Peter <psl4@tpu.ru>
2020-01-17 14:22:09 +0700

    Update file.js

--- part1/1.2/file.js
+++ part1/1.2/file.js
@@ -1,6 +1,5 @@
-const t = [1, -1, 3]
-const m1 = t.map(value => value * 2)
-console.log(m1)
+const t = [1, 2, 3, 4, 5]
 
-const m2 = t.map(value => '<li>' + value + '</li>')
-console.log(m2)
+const [first, second, ...rest] = t
+console.log(first, second)
+console.log(rest)
