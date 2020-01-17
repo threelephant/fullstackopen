041200d01a0e813e2fbc8d94c114e18239ad5f87
Peter <psl4@tpu.ru>
2020-01-17 14:33:56 +0700

    Update file.js

--- part1/1.2/file.js
+++ part1/1.2/file.js
@@ -1,27 +1,25 @@
-const object1 = {
-    name: 'Arto Hellas',
-    age: 35,
-    education: 'PhD',
+const sum = (p1, p2) => {
+    console.log(p1)
+    console.log(p2)
+    return p1 + p2
 }
 
-const object2 = {
-    name: 'Full Stack web application development',
-    level: 'intermediate studies',
-    size: 5,
+const result = sum(1, 5)
+console.log(result)
+
+const square = p => p * p
+
+const t = [1, 2, 3]
+const tSquared = t.map(p => p * p)
+
+function product(a, b) {
+    return a * b
 }
 
-const object3 = {
-    name: {
-        first: 'Dan',
-        last: 'Abramov',
-    },
-    grades: [2, 3, 5, 3],
-    department: 'Standford University',
+const result2 = product(2, 6)
+
+const average = function(a, b) {
+    return (a + b) / 2
 }
 
-console.log(object1.name)
-const fieldName = 'age'
-console.log(object1[fieldName])
-
-object1.address = 'Helsinki'
-object1['secret number'] = 12341
+const result3 = average(2, 5)
