b4db402518a653efc041c93a10c73cdffe1a6e5d
Peter <psl4@tpu.ru>
2020-01-17 15:37:03 +0700

    Update index.js

--- part1/1.1/src/index.js
+++ part1/1.1/src/index.js
@@ -32,10 +32,20 @@ const Footer = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development'
-    const parts = ['Fundamentals of React',
-                    'Using props to pass data',
-                    'State of a component']
-    const exercises = [10, 7, 14]
+    const part1 = {
+        name: 'Fundamentals of React', 
+        exercises: 10    
+    }
+    const part2 = {
+        name: 'Using props to pass data', 
+        exercises: 7    
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
+    const parts = [part1.name, part2.name, part3.name]
+    const exercises = [part1.exercises, part2.exercises, part3.exercises]
 
     return (
         <div>
