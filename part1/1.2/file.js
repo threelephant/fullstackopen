6a4c52dc5f5e94f6bf33b877ef863bae4b98b19d
Peter <psl4@tpu.ru>
2020-01-17 14:25:12 +0700

    Update file.js

--- part1/1.2/file.js
+++ part1/1.2/file.js
@@ -1,5 +1,20 @@
-const t = [1, 2, 3, 4, 5]
+const object1 = {
+    name: 'Arto Hellas',
+    age: 35,
+    education: 'PhD',
+}
 
-const [first, second, ...rest] = t
-console.log(first, second)
-console.log(rest)
+const object2 = {
+    name: 'Full Stack web application development',
+    level: 'intermediate studies',
+    size: 5,
+}
+
+const object3 = {
+    name: {
+        first: 'Dan',
+        last: 'Abramov',
+    },
+    grades: [2, 3, 5, 3],
+    department: 'Standford University',
+}
