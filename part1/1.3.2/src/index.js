83641cc1b49df0c70ad05f6bb073f449cab3bab1
Peter <psl4@tpu.ru>
2020-01-17 21:16:45 +0700

    Update index.js

--- part1/1.3.2/src/index.js
+++ part1/1.3.2/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = (props) => {
+    const {counter} = props
+    return (
+        <div>{counter}</div>
+    )
+}
+
+let counter = 1
+
+ReactDOM.render {
+    <App counter={counter}/>,
+    document.getElementById('root')
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
