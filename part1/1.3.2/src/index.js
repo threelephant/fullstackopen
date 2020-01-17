a08feeed6ef9af4389e2363ee1d59e60705e2e08
Peter <psl4@tpu.ru>
2020-01-17 21:46:38 +0700

    Update index.js

--- part1/1.3.2/src/index.js
+++ part1/1.3.2/src/index.js
@@ -4,15 +4,19 @@ import ReactDOM from 'react-dom';
 const App = (props) => {
     const [ counter, setCounter ] = useState(0)
 
-    const setToValue = (value) => setCounter(value)
+    const setToValue = (value) => {
+        return () => {
+            setCounter(value)
+        }
+    }
 
     return (
         <div>
             <div>{counter}</div>
-            <button onClick={() => setToValue(counter + 1)}>
+            <button onClick={setToValue(counter + 1)}>
                 plus
             </button>
-            <button onClick={() => setToValue(0)}>
+            <button onClick={setToValue(0)}>
                 zero
             </button>
         </div>
