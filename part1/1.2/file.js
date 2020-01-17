ef0ecd74bbcd691f54ad50ace5a90f1d9541df9e
Peter <psl4@tpu.ru>
2020-01-17 20:33:30 +0700

    Update file.js & Create new react-app

--- part1/1.2/file.js
+++ part1/1.2/file.js
@@ -54,4 +54,20 @@ arto.growOlder = function() {
 // const referenceToGreet = arto.greet
 // referenceToGreet()
 
-setTimeout(arto.greet.bind(arto), 1000)
+// setTimeout(arto.greet.bind(arto), 1000)
+
+class Person {
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+    greet() {
+        console.log('hello, my name is', this.name)
+    }
+}
+
+const adam = new Person('Adam Ondra', 35)
+adam.greet()
+
+const janja = new Person('Janja Garnbret', 22)
+janja.greet()
