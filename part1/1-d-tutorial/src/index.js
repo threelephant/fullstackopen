6e7165ccad497a59c67d78372000fb1b60b56c8c
Peter <threelephant@gmail.com>
2020-01-20 19:32:47 +0700

    Update index.js

--- part1/1-d-tutorial/src/index.js
+++ part1/1-d-tutorial/src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const App = (props) => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+
+    return (
+        <div>
+            <div>
+                {left}
+                <button onClick={() => setLeft(left + 1)}>
+                    left
+                </button>
+                <button onClick={() => setRight(right + 1)}>
+                    right
+                </button>
+                {right}
+            </div>
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
