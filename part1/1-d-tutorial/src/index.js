ef89cee0564542710246c55adaf6c31fc61507a1
Peter <threelephant@gmail.com>
2020-01-20 19:48:24 +0700

    Update index.js

--- part1/1-d-tutorial/src/index.js
+++ part1/1-d-tutorial/src/index.js
@@ -3,20 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const App = (props) => {
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
+    const [clicks, setClicks] = useState({
+        left: 0, right: 0
+    })
+
+    const handleLeftClick = () => {
+        const newClicks = {
+            ...clicks,
+            left: clicks.left + 1
+        }
+        setClicks(newClicks)
+    }
+
+    const handleRightClick = () => {
+        const newClicks = {
+            ...clicks,
+            right: clicks.right + 1
+        }
+        setClicks(newClicks)
+    }
 
     return (
         <div>
             <div>
-                {left}
-                <button onClick={() => setLeft(left + 1)}>
-                    left
-                </button>
-                <button onClick={() => setRight(right + 1)}>
-                    right
-                </button>
-                {right}
+                {clicks.left}
+                <button onClick={handleLeftClick}>left</button>
+                <button onClick={handleRightClick}>right</button>
+                {clicks.right}
             </div>
         </div>
     )
