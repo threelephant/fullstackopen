ef88afde0fb5fbdbe68bfa40a443bab0a620ca5e
threelephant <threelephant@gmail.com>
2020-02-21 12:04:44 +0700

    Complete exercise 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -25,6 +25,10 @@ const App = () => {
     setBad(bad + 1)
   }
 
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all * 100
+
   return (
     <>
       <h2>give feedback</h2>
@@ -35,6 +39,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </>
   )
 }
