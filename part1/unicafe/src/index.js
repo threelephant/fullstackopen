a257fee05d3479fb07ffaded5d64003c05eff0e6
threelephant <threelephant@gmail.com>
2020-02-21 11:57:03 +0700

    Created unicafe
    * Complete first exercise

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodClick = () => {
+    setGood(good + 1)
+  }
+
+  const neutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const badClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <>
+      <h2>give feedback</h2>
+      <Button onClick={goodClick} text="good" />
+      <Button onClick={neutralClick} text="neutral" />
+      <Button onClick={badClick} text="bad" />
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
