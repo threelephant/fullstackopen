4580cd135a961583325d8f2bcdfdf0c947ef1989
threelephant <threelephant@gmail.com>
2020-02-22 18:06:36 +0700

    Complete 2.4

--- part2/course_contents/src/index.js
+++ part2/course_contents/src/index.js
@@ -12,6 +12,7 @@ const Part = (props) => (
 const Content = (props) => {
   const parts = () => props.parts.map(part =>
     <Part 
+      key={part.id}
       part={part.name} 
       exercise={part.exercises}
     />
@@ -32,6 +33,7 @@ const Footer = (props) => {
 }
 
 const Course = ({ course }) => {
+
   return (
     <>
       <Header course={course.name} />
@@ -42,31 +44,61 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React', 
-        exercises: 10    
-      },
-      {
-        name: 'Using props to pass data', 
-        exercises: 7    
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-      {
-        name: 'Redux',
-        exercises: 11
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React', 
+          exercises: 10,
+          id: 1    
+        },
+        {
+          name: 'Using props to pass data', 
+          exercises: 7,
+          id: 2    
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing', 
+          exercises: 3,
+          id: 1    
+        },
+        {
+          name: 'Middlewares', 
+          exercises: 7,
+          id: 2    
+        }
+      ]
+    }
+  ]
+
+  const course = () => courses.map(course =>
+    <Course
+      key={course.id} 
+      course={course}
+    />
+  )
 
   return (
     <div>
-      <Course course={course} />
+      {course()}
     </div>
   )
 }
