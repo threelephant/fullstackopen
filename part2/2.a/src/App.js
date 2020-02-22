c63e85ffe97c69d28530df660baddcff421e24a8
threelephant <threelephant@gmail.com>
2020-02-22 18:31:28 +0700

    Added handleNoteChange

--- part2/2.a/src/App.js
+++ part2/2.a/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+
   const rows = () => notes.map(note =>
     <Note
       key={note.id}
       note={note}
     />
   )
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
   
   return (
     <>
@@ -15,6 +30,15 @@ const App = ({ notes }) => {
       <ul>
         {rows()}
       </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">
+          save
+        </button>
+      </form>
     </>
   )
 }
