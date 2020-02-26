294e197748621482b46f19fb67b62cd1eb258793
threelephant <threelephant@gmail.com>
2020-02-27 00:01:48 +0700

    Complete 2e tutorial

--- part2/2.c/src/App.js
+++ part2/2.c/src/App.js
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/ErrorNotification'
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2019</em>
+    </div>
+  )
+}
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
@@ -19,18 +36,21 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
-  const toggleImportance = id => {
+  const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = {...note, important: !note.important}
 
     noteService
       .update(id, changedNote).then(returnedNote => {
-      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
     })
     .catch(error => {
-      alert(
-        `the note '${note.content}' was already deleted from server`
+      setErrorMessage(
+        `Note '${note.content} was already removed from server'`
       )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
       setNotes(notes.filter(n => n.id !== id))
     })
   }
@@ -39,7 +59,7 @@ const App = () => {
     <Note
       key={note.id}
       note={note}
-      toggleImportance={() => toggleImportance(note.id)}
+      toggleImportance={() => toggleImportanceOf(note.id)}
     />
   )
 
@@ -67,6 +87,9 @@ const App = () => {
   return (
     <>
       <h1>Notes</h1>
+
+      <Notification message={errorMessage} />
+
       <>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -84,6 +107,7 @@ const App = () => {
           save
         </button>
       </form>
+      <Footer />
     </>
   )
 }
--- part2/2.c/src/index.js
+++ part2/2.c/src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import './index.css'
 import App from './App'
 
 ReactDOM.render(
