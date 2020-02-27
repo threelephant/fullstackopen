9959adade60ed976b1d39fd7d10331d0a1ed509a
threelephant <threelephant@gmail.com>
2020-02-27 21:47:50 +0700

    Complete 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -45,6 +45,13 @@ app.get('/info', (req, res) => {
   `)
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
