ef18476f4775822cbee8ebd15d1707c76ad77d6e
threelephant <threelephant@gmail.com>
2020-02-28 00:37:50 +0700

    Complete 3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 app.use(bodyParser.json())
+app.use(morgan('combined'))
 
 let persons = [
   {
